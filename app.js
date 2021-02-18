f0f3757c3e1b6e87df12afe4be145119c7abbfac
devian5 <acgarage@outlook.es>
2021-02-18 16:50:29 +0100

    add restful router

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
-
+const router = require('./routers/carRouter')
 const app = express()
 
 const port = 3000
 
-
+app.use(router)
+app.use(express.json());
 
 app.listen(port, () =>  {
     console.log('Ya estoy en marcha')
--- routers/carRouter.js
+++ routers/carRouter.js
@@ -1,4 +1,37 @@
-const router = require('express').Router();
+const router = require('express').Router()
 const carController = require('../controllers/car');
 
-router.get('/allCar', carController.indexAll)
+
+// router.get('/cars', (req, res) => res.send('hello!'));
+
+const callback = async (req,res) => {
+    res.send(carController.indexAll())
+}
+
+router.get('/cars', callback);
+
+router.post('/cars', async (req, res)=> {
+    try {
+        console.log(req.body.json())
+        res.json(await carController.store(req.body));
+       
+   } catch (error) {
+       return res.sendStatus(500).send({
+           message: 'oops!'
+       })
+   }
+})
+
+router.put('/cars/:id', async (req,res)=>{
+    const id = req.params.id;
+    res.json(await carController.update(id,req.body));
+});
+
+router.delete('/cars/:id', async (req, res)=> {
+    const id = req.params.id;
+    res.json(await carController.destroy(id));
+});
+
+module.exports = router;
+
+
